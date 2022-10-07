f46e2c2bb7a4449599d4361d1ea459f0dfa6b76c
Madebybrown <madebybrown@gmail.com>
2022-10-07 10:50:00 +0000

    Add welcome message, + name input

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+if __name__ == "__main__":
+    while True:
+        print("Welcome to Riddles Adventure!")
+        print("As an adventurer you've decided to visit the maze of Himoshi.")
+        print("But while inside you lose track of time,\n you find yourself lost.")
+        print("You can choose to walk in multiple directions.")
+        print("Hope you'll find a way out!")
+        print("What's your name?: ")
+        name = input()
+        print("Good luck, " + name + ".")
+        break
