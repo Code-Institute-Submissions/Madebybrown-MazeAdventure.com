c122109c1d2e1dea9f6835b919fcd865f9c75454
Madebybrown <madebybrown@gmail.com>
2022-10-07 11:55:28 +0000

    Add while loop to startingRoom function in run.py

--- run.py
+++ run.py
@@ -3,28 +3,47 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 def startingRoom():
+    """
+    Creates a loop that repeats the options if the users 
+    input is not in directions. (Example - backward)
+    """
+    directions = ["left", "right", "forward"]
     print("\nYou find yourself in the middle of a room, there are four paths to chose from, where would you like to go?")
+    userInput = ""
+    
+    while userInput not in directions:
+        print("Options: left | right | backward | forward")
+        if userInput == "left":
+            riddleRoomOne()
+        elif userInput == "right":
+            deathRoom()
+        elif userInput == "backward":
+            print("You find that this way in blocked by heavy rocks, you have to turn around and take another way.")
+        elif userInput == "forward":
+            riddleRoomTwo()
+        else:
+            print("Please enter a valid option.")
 
-def deathRoom():
+# def deathRoom():
 
-def emptyRoom():
+# def emptyRoom():
 
-def riddleRoomOne():
+# def riddleRoomOne():
 
-def riddleRoomTwo():
+# def riddleRoomTwo():
 
-def riddleRoomThree():
+# def riddleRoomThree():
 
-def riddleRoomFour():
+# def riddleRoomFour():
 
-def deadEnd():
+# def deadEnd():
 
 if __name__ == "__main__":
     while True:
         print("Welcome to Riddles Adventure!")
         print("\nAs an adventurer you've decided to visit the maze of Himoshi.")
-        print("But while inside you lose track of time,\n you find yourself lost.")
-        print("You can choose to walk in multiple directions.")
+        print("But while inside you lose track of time,\n and you find yourself lost.")
+        print("\nYou can choose to walk in multiple directions.")
         print("But beware! \nChallanges will present themselves, Hope you'll find a way out!")
         print("What's your name?: ")
         name = input()
